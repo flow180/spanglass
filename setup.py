e17085efa35df0b0f6544758ce53c559ff383a2b
Charlie Wolf <charlie.wolf@beatport.com>
2015-10-22 13:00:53 -0500

    add reqs

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from pip.req import parse_requirements
+import os
 
 setup(
     name='spanglass',
     version='0.0.1',
     description='Static site deployment tool',
+    install_requires = ['six', 'formic', 'cement', 'boto'],
     platforms='Platform Independent',
     author='Charlie Wolf',
     author_email='charlie@wolf.is',
